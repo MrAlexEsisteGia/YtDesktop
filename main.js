f59284a5c47441798129c914b573d970f9533087
MrAlex <24595840+MrAlexEsisteGia@users.noreply.github.com>
2019-03-01 20:50:41 +0100

    First thingy

---
+++ main.js
@@ -0,0 +1,21 @@
+const {app, BrowserWindow} = require('electron')
+let mainWindow
+function createWindow () {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+  mainWindow.setMenuBarVisibility(false)
+  mainWindow.loadFile('index.html')
+  mainWindow.on('closed', function () {
+    mainWindow = null
+  })
+}
+app.on('ready', createWindow)
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }})
