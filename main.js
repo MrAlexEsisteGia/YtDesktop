7c3e452b4bf7326269f470438228f709f6925785
MrAlex <24595840+MrAlexEsisteGia@users.noreply.github.com>
2019-03-02 23:38:35 +0100

    Added really basic tray support

--- main.js
+++ main.js
@@ -1,19 +1,28 @@
-const {app, BrowserWindow} = require('electron')
+const {app, BrowserWindow, Menu, Tray} = require('electron')
+
 let mainWindow
 function createWindow () {
   mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
     webPreferences: {
       nodeIntegration: true
-    }
-  })
+    }})
+	
   mainWindow.setMenuBarVisibility(false)
   mainWindow.loadURL('https://music.youtube.com/')
   mainWindow.on('closed', function () {
     mainWindow = null
-  })
-}
+  })}
+  
+  let tray = null
+app.on('ready', () => {
+  tray = new Tray('icon.ico')
+  const contextMenu = Menu.buildFromTemplate([
+    { label: 'Youtube Music', type: 'radio', checked: true },
+  ])
+  tray.setToolTip('Yt Desktop')
+  tray.setContextMenu(contextMenu)
+})
+  
 app.on('ready', createWindow)
 app.on('window-all-closed', function () {
   if (process.platform !== 'darwin') {
