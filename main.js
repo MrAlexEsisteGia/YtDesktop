941fb3a9116cb79732816447d73a30d11f0d7cd5
italiano.amazing.py <bildcraft10000@gmail.com>
2019-03-09 11:07:11 +0100

    icons
    Added icons, i will repack the app for the  icons in the .exe version

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 const {app, BrowserWindow} = require('electron')
+var path = require('path')
 let mainWindow
 function createWindow () {
   mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
+	icon: path.join(__dirname, 'assets/icons/png/512x512.png'),
     webPreferences: {
       nodeIntegration: true
     }
