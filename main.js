11e8f908f0fc50ba5181381181e504cdf331a2c0
italiano.amazing.py <bildcraft10000@gmail.com>
2019-03-09 11:19:43 +0100

    Icons
    added icons + tray icons (i will repack the app for the icons)

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 const {app, BrowserWindow, Menu, Tray} = require('electron')
+var path = require('path')
 
 let mainWindow
 function createWindow () {
   mainWindow = new BrowserWindow({
+	icon: path.join(__dirname, 'assets/icons/png/512x512.png'),
     webPreferences: {
       nodeIntegration: true
     }})
@@ -15,7 +17,7 @@ function createWindow () {
   
   let tray = null
 app.on('ready', () => {
-  tray = new Tray('icon.png')
+  tray = new Tray('assets/icons/png/512x512.png')
   const contextMenu = Menu.buildFromTemplate([
     { label: 'Youtube Music', type: 'radio', checked: true },
   ])
@@ -27,4 +29,4 @@ app.on('ready', createWindow)
 app.on('window-all-closed', function () {
   if (process.platform !== 'darwin') {
     app.quit()
-  }})
+}})
