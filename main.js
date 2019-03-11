60b36069e4e2a16d5f66f7089ae2972bf54d9af6
MrAlex <24595840+MrAlexEsisteGia@users.noreply.github.com>
2019-03-11 11:06:41 +0100

    easier to read main code

--- main.js
+++ main.js
@@ -10,12 +10,14 @@ function createWindow () {
     }})
 	
   mainWindow.setMenuBarVisibility(false)
+	
   mainWindow.loadURL('https://music.youtube.com/')
+	
   mainWindow.on('closed', function () {
     mainWindow = null
   })}
-  
-  let tray = null
+
+let tray = null
 app.on('ready', () => {
   tray = new Tray('assets/icons/png/256x256.png')
   const contextMenu = Menu.buildFromTemplate([
@@ -24,8 +26,9 @@ app.on('ready', () => {
   tray.setToolTip('Yt Desktop')
   tray.setContextMenu(contextMenu)
 })
-  
+
 app.on('ready', createWindow)
+
 app.on('window-all-closed', function () {
   if (process.platform !== 'darwin') {
     app.quit()
