adbc129146085c86d4a30572e4480db2e8774edc
MrAlex <24595840+MrAlexEsisteGia@users.noreply.github.com>
2019-03-14 04:53:35 +0100

    Massive code overhaul
    Startup logo
    Tray icon that hides the app

--- README.md
+++ README.md
@@ -6,21 +6,25 @@
 Windows:           
 ----------------------          
 Devs:            
-```
-Open "install.bat" to Install          
-and "start.bat" to open electron            
+```   
+Make sure to have NodeJs installed         
+Download and UnZip the repository      
+Open "install.bat" to Install           
+and "start.bat" to open YtDesktop            
 ```
 Users:               
 ```
-Go to releases and download "YtDesktop-win32-x64.zip"         
-Unzip it, and open "YtDesktop.exe"                   
-```
+Go to releases and download "YtDesktop-win32-x64.zip"          
+Unzip it, and open "YtDesktop.exe"                    
+```           
 Linux:          
-------------          
-Devs:           
-```
+------------            
+Devs:            
+```           
+Make sure to have NodeJs installed           
+Download and UnZip the repository          
 Open "install.sh" to Install          
-and "start.sh" to open electron            
+and "start.sh" to open YtDesktop            
 ```
 Users:         
 ```
--- main.js
+++ main.js
@@ -2,33 +2,58 @@ const {app, BrowserWindow, Menu, Tray} = require('electron')
 var path = require('path')
 
 let mainWindow
+let loadingWindow
 function createWindow () {
+  loadingWindow = new BrowserWindow({
+	backgroundColor: '#00FFFFFF',
+    width: 256,
+    height: 256,
+    transparent: true,
+	resizable: false,
+	alwaysOnTop: true,
+    frame:false
+  })
+  loadingWindow.setMenuBarVisibility(false)
+  loadingWindow.loadFile('assets/icons/png/icon.html')
+  
+
   mainWindow = new BrowserWindow({
-	icon: path.join(__dirname, 'assets/icons/png/256x256.png'),
+	icon: ('assets/icons/png/512x512.png'),
+	backgroundColor: '#000000',
+	show: false,
     webPreferences: {
       nodeIntegration: true
     }})
-	
+
+    mainWindow.once('ready-to-show', () => {
+     mainWindow.show()
+	 mainWindow.maximize()
+	 loadingWindow.close()
+    })
+
   mainWindow.setMenuBarVisibility(false)
-	
   mainWindow.loadURL('https://music.youtube.com/')
-	
+
   mainWindow.on('closed', function () {
-    mainWindow = null
-  })}
+   mainWindow = null
+  })
 
 let tray = null
-app.on('ready', () => {
-  tray = new Tray('assets/icons/png/256x256.png')
-  const contextMenu = Menu.buildFromTemplate([
-    { label: 'Youtube Music', type: 'radio', checked: true },
-  ])
-  tray.setToolTip('Yt Desktop')
-  tray.setContextMenu(contextMenu)
+mainWindow.once('ready-to-show', () => {
+tray = new Tray('assets/icons/png/256x256.png')
+tray.setToolTip('Yt Desktop')
+tray.on('click', () => {
+ if(mainWindow.isVisible()){
+  mainWindow.hide()
+  tray.setImage('assets/icons/png/256x256mono.png')
+} else {
+  mainWindow.show()
+  tray.setImage('assets/icons/png/256x256.png')
+}
 })
-
+})
+}
 app.on('ready', createWindow)
-
 app.on('window-all-closed', function () {
   if (process.platform !== 'darwin') {
     app.quit()
