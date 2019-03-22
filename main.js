52ddebd4a2f378f26f9a9444f8f28cf7ddca6267
MrAlex <24595840+MrAlexEsisteGia@users.noreply.github.com>
2019-03-22 15:05:05 +0100

    1.0pre3
    notifications
    and boot animation

--- main.js
+++ main.js
@@ -1,65 +1,106 @@
-const {app, BrowserWindow, Menu, Tray, nativeImage} = require('electron')
+const {
+  app,
+  BrowserWindow,
+  Tray,
+  nativeImage,
+  globalShortcut
+} = require('electron')
 var path = require('path')
 
-var icon = path.join(__dirname, 'assets/icons/256x256.png');
-const trayimage = nativeImage.createFromPath(icon);
-var grayicon = path.join(__dirname, 'assets/icons/256x256mono.png');
-const graytrayimage = nativeImage.createFromPath(grayicon);
+var icon = path.join(__dirname, 'assets/icons/256x256.png')
+const trayimage = nativeImage.createFromPath(icon)
+var grayicon = path.join(__dirname, 'assets/icons/256x256mono.png')
+const graytrayimage = nativeImage.createFromPath(grayicon)
 
+let oldtitle
+let tray = null
+let notificationWindow
 let mainWindow
 let loadingWindow
 function createWindow () {
+  // startup screen
   loadingWindow = new BrowserWindow({
-	backgroundColor: '#00FFFFFF',
+    backgroundColor: '#00FFFFFF',
     width: 256,
     height: 256,
     transparent: true,
-	resizable: false,
-	alwaysOnTop: true,
-    frame:false
+    resizable: false,
+    alwaysOnTop: true,
+    frame: false,
+    skipTaskbar: true
   })
   loadingWindow.setMenuBarVisibility(false)
   loadingWindow.loadFile('assets/icons/icon.html')
-  
 
+  // main window
   mainWindow = new BrowserWindow({
-	icon: (icon),
-	backgroundColor: '#000000',
-	show: false,
-    webPreferences: {
-      nodeIntegration: true
-    }})
-
-    mainWindow.once('ready-to-show', () => {
-     mainWindow.show()
-	 mainWindow.maximize()
-	 loadingWindow.close()
+    icon: icon,
+    backgroundColor: '#000000',
+    show: false,
+    MenuBarVisibility: false
+  })
+  mainWindow.once('ready-to-show', () => {
+    mainWindow.show()
+    mainWindow.maximize()
+    loadingWindow.close()
+    globalShortcut.register('Alt+x', () => {
+      if (mainWindow.getTitle() !== 'YouTube Music') {
+        notificationWindow.webContents.executeJavaScript(
+          'changesong("' + mainWindow.getTitle().slice(0, -16) + '");'
+        )
+      } else {
+        notificationWindow.webContents.executeJavaScript(
+          'changesong(" Paused ");'
+        )
+      }
     })
-
+  })
   mainWindow.setMenuBarVisibility(false)
   mainWindow.loadURL('https://music.youtube.com/')
-
   mainWindow.on('closed', function () {
-   mainWindow = null
+    app.quit()
   })
 
-let tray = null
-mainWindow.once('ready-to-show', () => {
-tray = new Tray(trayimage)
-tray.setToolTip('Yt Desktop')
-tray.on('click', () => {
- if(mainWindow.isVisible()){
-  mainWindow.hide()
-  tray.setImage(graytrayimage)
-} else {
-  mainWindow.show()
-  tray.setImage(trayimage)
+  // notifications
+  notificationWindow = new BrowserWindow({
+    backgroundColor: '#00FFFFFF',
+    width: 1920,
+    height: 1080,
+    transparent: true,
+    resizable: false,
+    alwaysOnTop: true,
+    frame: false,
+    skipTaskbar: true,
+    type: 'toolbar'
+  })
+  notificationWindow.setIgnoreMouseEvents(true)
+  notificationWindow.setMenuBarVisibility(false)
+  notificationWindow.loadFile('assets/notifications/notification.html')
+  oldtitle = 'YouTube Music'
+  setInterval(function () {
+    if (oldtitle !== mainWindow.getTitle()) {
+      if (mainWindow.getTitle() !== 'YouTube Music') {
+        notificationWindow.webContents.executeJavaScript(
+          'changesong("' + mainWindow.getTitle().slice(0, -16) + '");'
+        )
+      }
+    }
+    oldtitle = mainWindow.getTitle()
+  }, 2000)
+
+  // tray
+  mainWindow.once('ready-to-show', () => {
+    tray = new Tray(trayimage)
+    tray.setToolTip('YtDesktop')
+    tray.on('click', () => {
+      if (mainWindow.isVisible()) {
+        mainWindow.hide()
+        tray.setImage(graytrayimage)
+      } else {
+        mainWindow.show()
+        tray.setImage(trayimage)
+      }
+    })
+  })
 }
-})
-})
-}
-app.on('ready', createWindow)
-app.on('window-all-closed', function () {
-  if (process.platform !== 'darwin') {
-    app.quit()
-}})
+app.on('ready', createWindow) // create main window
